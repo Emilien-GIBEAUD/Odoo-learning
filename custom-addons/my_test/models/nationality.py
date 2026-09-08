d3b2e4ac3d4101202aa73c6a8522b7461f8f2766
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2026-09-08 17:09:34 +0200

    Add fresh-clone branch.

--- custom-addons/my_test/models/nationality.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class Nationality(models.Model):
-    _name = 'my_test.nationality'
-    _description = 'Nationalité'
-
-    name = fields.Char(string='Nationalité', required=True)
